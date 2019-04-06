484e983966edf94d0deee8710e9d8e0907eed8af
Mark Polyak <markpolyak@users.noreply.github.com>
2019-04-06 17:57:16 +0300

    Add _WIN32 check for VS

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ std::random_device rd;     // only used once to initialise (seed) engine
 std::mt19937 rng(rd());    // random-number engine used (Mersenne-Twister in this case)
 std::uniform_int_distribution<int> uni(0, sleep_time*2); // guaranteed unbiased
 
-#ifdef WIN32
+#if defined WIN32 || defined _WIN32
 #include <windows.h>
 #elif _POSIX_C_SOURCE >= 199309L
 #include <time.h>   // for nanosleep
@@ -20,7 +20,7 @@ void sleep_ms(int milliseconds) // cross-platform sleep function
 {
     auto random_integer = uni(rng);
     milliseconds += random_integer;
-#ifdef WIN32
+#if defined WIN32 || defined _WIN32
     Sleep(milliseconds);
 #elif _POSIX_C_SOURCE >= 199309L
     struct timespec ts;
